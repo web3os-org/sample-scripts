306b6fc57fa8fd6d5eaea1c5c81789989752cdfe
Jay Mathis <code@mathis.network>
2022-05-06 21:30:26 -0500

    Update owner.js

--- contract/owner.js
+++ contract/owner.js
@@ -12,11 +12,11 @@
 
   terminal.log(`\n\nContract: ${address}`)
   terminal.log(`Setting Owner: ${newOwner}`)
-  await kernel.bin.contract.write(options)
+  await kernel.modules.contract.write(options)
 
   options.method = 'getOwner'
   options.args = []
-  const result = await kernel.bin.contract.read(options)
+  const result = await kernel.modules.contract.read(options)
 
   terminal.log(`New Owner: ${result}\n`)
   terminal.prompt()
