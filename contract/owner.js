6b88ce68359939b5eeec1caafe096548ada18ff4
Jay Mathis <code@mathis.network>
2022-09-20 03:41:54 -0500

    Update owner.js

--- contract/owner.js
+++ contract/owner.js
@@ -10,16 +10,16 @@
     abiUrl: 'https://raw.githubusercontent.com/web3os-org/sample-scripts/master/contract/owner.abi'
   }
 
-  terminal.log(`\n\nContract: ${address}`)
-  terminal.log(`Setting Owner: ${newOwner}`)
-  await kernel.modules.contract.write(options)
+  Terminal.log(`\n\nContract: ${address}`)
+  Terminal.log(`Setting Owner: ${newOwner}`)
+  await Kernel.modules.contract.write(options)
 
   options.method = 'getOwner'
   options.args = []
-  const result = await kernel.modules.contract.read(options)
+  const result = await Kernel.modules.contract.read(options)
 
-  terminal.log(`New Owner: ${result}\n`)
-  terminal.prompt()
+  Terminal.log(`New Owner: ${result}\n`)
+  Terminal.prompt()
   
-  kernel.dialog({ title: 'Owner Updated', text: result })
+  Kernel.dialog({ title: 'Owner Updated', text: result })
 })()
